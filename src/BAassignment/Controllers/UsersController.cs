4542f9482bbde486f94ec80d6af367d0a194131c
justasem <justasem@gmail.com>
2017-04-22 16:26:55 +0300

    Added Register and AllUsers Views; Added UsersController; Changed homepage in RouteConfig;

---
+++ src/BAassignment/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BAassignment.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Detail()
+        {
+            return View();
+            
+        }
+    }
+}
